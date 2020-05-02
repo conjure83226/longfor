e45e68e882b6f56de61661dabaf8e5bc2f1cbc25
邪光闪现 <conjure83226@sina.com>
2020-05-02 17:28:52 +0800

    Initial commit

---
+++ longfor/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  longfor
+//
+//  Created by jack on 2020/5/2.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        print("1111")
+        return true
+    }
+
+
+}
+
